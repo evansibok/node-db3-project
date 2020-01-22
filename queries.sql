8531ba5fbbb9a05bcca230d137d62b57c99d1c93
Evans Ibok <evansibok@gmail.com>
2020-01-22 14:22:20 +0100

    add two queries

--- queries.sql
+++ queries.sql
@@ -1,8 +1,23 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select
+    productname,
+    categoryname
+from product p
+left join category c
+on p.categoryid = c.id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+select 
+    o.id order_id,
+    companyname company_name,
+    o.orderdate order_date
+from [order] o
+join shipper s
+on o.shipvia = s.id
+where o.orderdate < "2012-08-09"
+order by o.orderdate desc;
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
